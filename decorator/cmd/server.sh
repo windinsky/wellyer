37efb2b797e8c68123ea09d806fc556821966aa8
windinsky <forzhp@gmail.com>
2013-11-18 11:52:57 +0800

    tst

--- decorator/cmd/server.sh
+++ decorator/cmd/server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-nohup node ../source/server.js 3000 >> 3000.log &
-nohup node ../source/server.js 3001 >> 3001.log &
-nohup node ../source/server.js 3002 >> 3002.log &
+nohup node ../source/server.js 3000 >> ../logs/3000.log &
+nohup node ../source/server.js 3001 >> ../logs/3001.log &
+nohup node ../source/server.js 3002 >> ../logs/3002.log &
